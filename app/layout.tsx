9ad465d6b8ebfc80c78257c8c7c3be0e69a20898
Rohan Rama Poojari <123969188+Cetyl@users.noreply.github.com>
2024-09-24 22:43:32 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,12 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
+    default: "Cetyl.com",
     template: "%s | chronark.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "Cetyl.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
     url: "https://chronark.com",
